895ad9db73f6096f93bdd6302ae1ef82dac0fbe2
王磊 <leiwang2396d@sf-express.com>
2019-05-05 11:52:33 +0800

    配置数据库

--- src/main/java/me/leig/task/controller/IndexController.java
+++ src/main/java/me/leig/task/controller/IndexController.java
@@ -12,11 +12,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -44,7 +41,6 @@ public class IndexController extends BaseController {
     }
 
     @RequestMapping(value = "login", method = {RequestMethod.POST})
-    @ResponseBody
     public String login(@RequestParam String username, @RequestParam String password, HttpServletResponse response) {
         Result<String> result = new Result<>();
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
